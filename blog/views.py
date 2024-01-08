d6f23ba5fdd619f25f21e180fc31575dc08a2e66
Sergey O <sa@osergey>
2024-01-09 00:01:28 +0300

    add blog, template

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic.base import View
+from .models import Post
 
-# Create your views here.
+class PostView(View):
+    """выводы записей"""
+    def get(self, request):
+        posts = Post.objects.all()
+        return render(request, 'blog/blog.html', {'post_list': posts})
+
