ac6668d5f7bdda2520a6c2138cd43f2ec5cef298
Sergey O <sa@osergey>
2024-01-10 09:04:37 +0300

    add img, style, template

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,7 @@ class Post(models.Model):
     description = models.TextField('Текст записи')
     author = models.CharField('Имя автора', max_length = 100)
     date = models.DateField('Дата публикации')
+    img = models.ImageField('Изображение', upload_to='image/%Y')
 
 def __str__(self):
     return f'{self.title}, {self.author}'
