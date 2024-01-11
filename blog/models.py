919cf99aff895cdfac561f12228fb89fd166af16
Sergey O <sa@osergey>
2024-01-12 00:19:16 +0300

    add comment, page post

--- blog/models.py
+++ blog/models.py
@@ -14,3 +14,17 @@ def __str__(self):
 class Meta:
     verbose_name = 'Запись'
     verbose_name_plural = 'Записи'
+
+class Comments(models.Model):
+    """Коментарии"""
+    email = models.EmailField()
+    name = models.CharField('Имя', max_length=50)
+    text_comments = models.TextField('Текст комментария', max_length=2000)
+    post = models.ForeignKey(Post, verbose_name='Публикация', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name}, {self.post}'
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
--- blog/views.py
+++ blog/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render
 from django.views.generic.base import View
 from .models import Post
 
-class PostView(View):
+class PostView( View ):
     """выводы записей"""
-    def get(self, request):
+    def get( self, request ):
         posts = Post.objects.all()
         return render(request, 'blog/blog.html', {'post_list': posts})
 
+class PostDetail( View ):
+    """ вывод записей """
+    def get(self, request, pk):
+        post = Post.objects.get( id = pk )
+        return render( request, 'blog/blog_detail.html', { 'post': post } )
